34734e0f4f8e6130c7e71c0936acd46347c644bf
takahiro <takapi854@gmail.com>
2022-11-21 22:15:53 +0900

    push

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # typescript-nextjs-aws-s3-cloudfront-terraform
 
-- node:v18.9.0 . 
-- 8.19.1 . 
+### Terraform
+ - s3 × cloudfront × OAC
+
+### Node.js
+- node:v18.9.0  
+- 8.19.1  
+
+
+### end point
+- https://dexxt2jlhrhsp.cloudfront.net/index.html 
---
+++ s3.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "cf-oac-with-tf-example"
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# バケットポリシー
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.s3_main_policy.json
+}
+
+data "aws_iam_policy_document" "s3_main_policy" {
+  # CloudFront Distribution からのアクセスのみ許可
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.main.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudfront_distribution.main.arn]
+    }
+  }
+}
